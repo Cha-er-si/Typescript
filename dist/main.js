ee293be8c92df38e3a7bc2a4208b36d1641fe8bd
Prince Velarde <53701801+Cha-er-si@users.noreply.github.com>
2022-10-23 17:10:57 +0800

    Initial Commit

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+// const a = "1";
+// console.log("aaa", a);
+/* -------------------- */
+/* Variables */
+// const hello = "world";
+// hello = "foo";
+// let hello = "world";
+// hello = "foo";
+// hello = true;
+// let hello: string = "world";
+// hello = true;
+/* -------------------- */
+/* Functions */
