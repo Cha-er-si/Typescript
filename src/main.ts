558398160ea8087087b5270fb9f0980b8ef6828e
Cha-er-si <vpcharles31@gmail.com>
2022-10-24 13:30:42 +0800

    Objects and Interfaces

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,41 @@
 //   return name + " " + surname;
 // };
 // console.log(getFullName("Prince", "Charles"));
-const getFullName = (name: string, surname: string): string => {
-  return name + " " + surname;
-};
-console.log(getFullName("Prince", "Charles"));
+// const getFullName = (name: string, surname: string): string => {
+//   return name + " " + surname;
+// };
+// console.log(getFullName("Prince", "Charles"));
+/* -------------------- */
+/* Objects */
+// interface User {
+//   name: string;
+//   age?: number;
+// }
+// const user: { name: string; age: number } = {
+//   name: "Moster",
+//   age: 13,
+// };
+// const user2: { name: string; age?: number } = {
+//   name: "Jack",
+// };
+// const user3: User = {
+//   name: "Jack",
+// };
+// console.log(user.name);
+/* -------------------- */
+/* Function in interfaces */
+// interface UserInterface {
+//   name: string;
+//   age?: number;
+//   getMessage(): string;
+// }
+// const user: UserInterface = {
+//   name: "Foster",
+//   age: 30,
+//   getMessage() {
+//     {
+//       return "Hello" + name;
+//     }
+//   },
+// };
+// console.log(user.getMessage());
