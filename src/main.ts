369a5162f58bb6441bd9f789fac95ec32a97fc1e
Cha-er-si <vpcharles31@gmail.com>
2022-10-24 16:26:02 +0800

    Generics

--- src/main.ts
+++ src/main.ts
@@ -197,3 +197,41 @@
 // console.log(User.maxAge);
 
 // const admin = new Admin("foo", "bar");
+
+/* -------------------- */
+/* Generics */
+const updatedArray = append<string>("bas", ["foo", "bar"]);
+const searchStr = "foo";
+const _hasSearchedString = any<string>(
+  (el: string) => el.contains(searchStr),
+  ["foooo", "bar", "baz"]
+);
+const addId = <T extends object>(obj: T) => {
+  const id = Math.random().toString(16);
+  return {
+    ...obj,
+    id,
+  };
+};
+
+interface UserInterface<T, V> {
+  name: string;
+  data: T;
+  meta: V;
+}
+
+const user: UserInterface<{ meta: string }, string> = {
+  name: "Jack",
+  data: {
+    meta: "foo",
+  },
+  meta: "bar",
+};
+
+const user: UserInterface<string[]> = {
+  name: "John",
+  data: ["foo", "bar", "baz"],
+};
+
+const result = addId<UserInterface>(user);
+console.log("result", result);
